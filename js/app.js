e67292b78dc911cbd19baa1644a072133494ac42
ElineJ <elinejacobse@gmail.com>
2020-04-22 15:02:47 -0400

    Feat: added helper function and made current section active on scroll

--- js/app.js
+++ js/app.js
@@ -17,7 +17,9 @@
  * Define Global Variables
  * 
 */
+
 const sections = document.querySelectorAll('section');
+const navbar = document.querySelector('.navbar__menu');
 
 /**
  * End Global Variables
@@ -25,10 +27,31 @@ const sections = document.querySelectorAll('section');
  * 
 */
 
-const navbar = document.querySelector('.navbar__menu');
-// for(section of sections) {
-    
-// }
+// Get the top and bottom Y coordinates of a given page section, returns an object
+function findRange(section) {
+    const range = {
+        top: section.offsetTop - 100, 
+        end: section.offsetTop + section.offsetHeight - 100
+    };
+    return range;
+}
+
+// Check if current position is within the range of a section, returns a boolean
+function inRange(curr, section) {
+    const range = findRange(section);
+    if(curr >= range.top && curr <= range.end) {
+        return true;
+    }
+    return false;
+}
+
+// Check if current element has the active class
+function isActive(section) {
+    if(section.classList.length > 0) {
+        return true;
+    }
+    else return false;
+}
 
 /**
  * End Helper Functions
@@ -48,9 +71,22 @@ for(section of sections) {
 
 
 // Add class 'active' to section when near top of viewport
+function addClass(event) {
+    const cursor = event.path[1].scrollY;
 
-
-
+    // check if section is in range
+    for(section of sections) {
+        if(inRange(cursor, section)) {
+            if(isActive(section)) {
+                return;
+            }
+            else {
+                document.querySelector('.now-active').classList.remove('now-active');
+                section.classList.add('now-active');
+            }
+        }
+    }
+}
 
 // Scroll to anchor ID using scrollTO event
 function smoothScroll(event) {
@@ -76,3 +112,4 @@ document.querySelector('#navbar__list').appendChild(fragment);
 navbar.addEventListener('click', smoothScroll);
 
 // Set sections as active
+document.addEventListener('scroll', addClass);
